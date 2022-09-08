c354c068826db036f56527bb50c494e07158ee1b
zakaria <erfan.yekezare@gmail.com>
2022-09-08 15:05:56 +0430

    Post, User viewsets instead of views.

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+# from rest_framework import generics
+from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 
 from .models import Post
@@ -6,23 +7,35 @@ from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class UserList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+# class PostList(generics.ListCreateAPIView):
+#    permission_classes = (IsAuthorOrReadOnly,)
+#    queryset = Post.objects.all()
+#    serializer_class = PostSerializer
+#
+#
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#    permission_classes = (IsAuthorOrReadOnly,)
+#    queryset = Post.objects.all()
+#    serializer_class = PostSerializer
+#
+#
+# class UserList(generics.ListCreateAPIView):
+#    queryset = get_user_model().objects.all()
+#    serializer_class = UserSerializer
+#
+#
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#    queryset = get_user_model().objects.all()
+#    serializer_class = UserSerializer
+#
