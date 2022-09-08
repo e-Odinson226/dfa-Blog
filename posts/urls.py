312dc5fd28355804ea75ae02f85d711a8841e530
zakaria <erfan.yekezare@gmail.com>
2022-09-08 15:16:08 +0430

    use rest_framework router instaed of,  simple path function routing

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+# from django.urls import path
+from rest_framework.routers import SimpleRouter
 
 from .views import *
 
-urlpatterns = [
-    path("users/", UserList.as_view(), name="user-list-api"),
-    path("users/<int:pk>", UserDetail.as_view(), name="user-detail-api"),
-    path("", PostList.as_view(), name="post-list-api"),
-    path("<int:pk>", PostDetail.as_view(), name="post-detail-api"),
-]
+router = SimpleRouter()
+router.register("", PostViewSet, basename="posts")
+router.register("users", UserViewSet, basename="users")
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#    path("users/", UserList.as_view(), name="user-list-api"),
+#    path("users/<int:pk>", UserDetail.as_view(), name="user-detail-api"),
+#    path("", PostList.as_view(), name="post-list-api"),
+#    path("<int:pk>", PostDetail.as_view(), name="post-detail-api"),
+# ]
