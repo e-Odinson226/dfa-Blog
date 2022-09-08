74d1fd6bc18eb2ce8020e7602d8848fab4d4c199
zakaria <erfan.yekezare@gmail.com>
2022-09-08 15:02:26 +0430

    create routes for user list and detail vies.

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path("users/", UserList.as_view(), name="user-list-api"),
+    path("users/<int:pk>", UserDetail.as_view(), name="user-detail-api"),
     path("", PostList.as_view(), name="post-list-api"),
     path("<int:pk>", PostDetail.as_view(), name="post-detail-api"),
 ]
