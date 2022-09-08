8ca770461ca2072d3d9776440c8cf7384125a0eb
zakaria <erfan.yekezare@gmail.com>
2022-09-08 14:44:36 +0430

    create seriaizer for user model.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Post
@@ -5,11 +6,11 @@ from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = (
-            "id",
-            "title",
-            "author",
-            "body",
-            "created_at",
-        )
+        fields = ("id", "title", "author", "body", "created_at")
         model = Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username")
