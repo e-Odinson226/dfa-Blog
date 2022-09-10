2a7f09ba0979b07726b1b18002c849016796cc50
zakaria <erfan.yekezare@gmail.com>
2022-09-10 18:37:17 +0430

    add secret key to env and read its value from there

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,8 @@ env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y3sv2np)dz$ec!&)+w*k+byb^j3pud#!9(n#e!zgp65e(+)cu^"
+# SECRET_KEY = "django-insecure-y3sv2np)dz$ec!&)+w*k+byb^j3pud#!9(n#e!zgp65e(+)cu^"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
